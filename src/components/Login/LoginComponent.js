43f64aceb0beccd0fc22d18fac0154c699793a84
Amaan Baiyat <amaan_baiyat@live.com>
2019-01-26 01:56:59 -0600

    Updated Layouts for Login and Register components

--- src/components/Login/LoginComponent.js
+++ src/components/Login/LoginComponent.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import {
+  Button, Form, Grid, Header, Message, Segment,
+} from 'semantic-ui-react';
+import style from './Login.less';
 
-const RegisterComponent = () => (
-  <div>
-    <h1>Login</h1>
+const LoginComponent = () => (
+  <div className={style.loginForm}>
+    <Grid textAlign="center" verticalAlign="middle">
+      <Grid.Column className={style.loginContainer}>
+        <Header color="blue">Log-in to your account</Header>
+        <Form size="large">
+          <Segment stacked>
+            <Form.Input
+              fluid
+              icon="user"
+              iconPosition="left"
+              placeholder="Username"
+            />
+            <Form.Input
+              fluid
+              icon="lock"
+              iconPosition="left"
+              placeholder="Password"
+              type="password"
+            />
+            <Button color="blue" fluid size="large">Login</Button>
+          </Segment>
+        </Form>
+        <Message>
+          Need an account?
+          {' '}
+          <a href="/register">Sign Up</a>
+        </Message>
+      </Grid.Column>
+    </Grid>
   </div>
 );
 
-export default RegisterComponent;
+export default LoginComponent;
--- src/components/Register/RegisterComponent.js
+++ src/components/Register/RegisterComponent.js
@@ -1,8 +1,52 @@
 import React from 'react';
+import {
+  Button, Form, Grid, Header, Message, Segment,
+} from 'semantic-ui-react';
+import style from './Register.less';
+
 
 const Register = () => (
-  <div>
-    <h1>Register</h1>
+  <div className={style.registerForm}>
+    <Grid textAlign="center" verticalAlign="middle">
+      <Grid.Column className={style.registerContainer}>
+        <Header color="blue">Create a new account</Header>
+        <Form size="large">
+          <Segment stacked>
+            <Form.Input
+              fluid
+              icon="user outline"
+              iconPosition="left"
+              placeholder="Name"
+            />
+            <Form.Input
+              fluid
+              icon="at"
+              iconPosition="left"
+              placeholder="Email"
+            />
+            <Form.Input
+              fluid
+              icon="user"
+              iconPosition="left"
+              placeholder="Username"
+            />
+            <Form.Input
+              fluid
+              icon="lock"
+              iconPosition="left"
+              placeholder="Password"
+              type="password"
+            />
+            <Button color="blue" fluid size="large">Register</Button>
+          </Segment>
+        </Form>
+        <Message>
+          Already have an account?
+          {' '}
+          <a href="/login">Log-in</a>
+        </Message>
+      </Grid.Column>
+    </Grid>
   </div>
 );
 
